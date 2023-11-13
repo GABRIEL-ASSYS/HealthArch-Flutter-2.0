8969b0bd79b56e1b47f8b0b5024efccc3aa4e860
Gabriel Assys Mendes Oliveira <110845125+GABRIEL-ASSYS@users.noreply.github.com>
2023-11-13 10:35:48 -0300

    corrigindo adicionar_consultas_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:health_arch/screens/Home/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MaterialApp(
-      home: const HomeScreen(),
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
-        scaffoldBackgroundColor: Colors.white,
-      ),
-    ));
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  runApp(MaterialApp(
+    home: const HomeScreen(),
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primarySwatch: Colors.lightBlue,
+      scaffoldBackgroundColor: Colors.white,
+    ),
+  ));
+}
