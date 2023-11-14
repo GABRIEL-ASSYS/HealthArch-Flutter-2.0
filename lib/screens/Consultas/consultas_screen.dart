d3b70042bcf8e99701b077a45d08f0c7ed408878
Gabriel Assys Mendes Oliveira <110845125+GABRIEL-ASSYS@users.noreply.github.com>
2023-11-14 08:58:58 -0300

    Update consultas_screen.dart

--- lib/screens/Consultas/consultas_screen.dart
+++ lib/screens/Consultas/consultas_screen.dart
@@ -1,86 +1,120 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:health_arch/screens/Consultas/adicionar_consultas_screen.dart';
-import 'package:health_arch/screens/Consultas/editar_consultas_screen.dart';
+import 'package:intl/intl.dart';
 
-class ConsultasScreen extends StatefulWidget {
-  const ConsultasScreen({Key? key}) : super(key: key);
+class EditarConsultaScreen extends StatefulWidget {
+  final String consultaId;
+
+  const EditarConsultaScreen({Key? key, required this.consultaId}) : super(key: key);
 
   @override
-  ConsultasScreenState createState() => ConsultasScreenState();
+  EditarConsultaScreenState createState() => EditarConsultaScreenState();
 }
 
-class ConsultasScreenState extends State<ConsultasScreen> {
-  late List<Map<String, dynamic>> consultas;
+class EditarConsultaScreenState extends State<EditarConsultaScreen> {
+  TextEditingController tituloController = TextEditingController();
+  TextEditingController descricaoController = TextEditingController();
+  TextEditingController horaDataController = TextEditingController();
+  TextEditingController nomeClienteController = TextEditingController();
+  TextEditingController nomeProfissionalController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    loadConsultas();
+    loadConsultaExistente();
   }
 
-  Future<void> loadConsultas() async {
+  Future<void> loadConsultaExistente() async {
     try {
-      QuerySnapshot querySnapshot =
-          await FirebaseFirestore.instance.collection('consultas').get();
+      DocumentSnapshot consultaSnapshot = await FirebaseFirestore.instance
+          .collection('consulta')
+          .doc(widget.consultaId)
+          .get();
 
-      consultas = querySnapshot.docs.map((DocumentSnapshot doc) {
-        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-        return data;
-      }).toList();
+      Map<String, dynamic> consultaData = consultaSnapshot.data() as Map<String, dynamic>;
 
-      setState(() {});
+      tituloController.text = consultaData['titulo'];
+      descricaoController.text = consultaData['descricao'];
+      horaDataController.text = DateFormat('yyyy-MM-dd HH:mm').format(
+        (consultaData['horaData'] as Timestamp).toDate(),
+      );
+      nomeClienteController.text = consultaData['nomeCliente'];
+      nomeProfissionalController.text = consultaData['nomeProfissional'];
     } catch (error) {
-      print('Erro ao carregar consultas: $error');
+      print('Erro ao carregar consulta existente: $error');
     }
   }
-  // Restante do seu código para excluir e editar consultas...
 
-  Future<void> excluirConsulta(String consultaId) async {
+  Future<void> editarConsultaExistente() async {
     try {
+      DateTime dataHora = DateTime.parse(horaDataController.text);
+      Timestamp timestamp = Timestamp.fromDate(dataHora);
+
       await FirebaseFirestore.instance
-          .collection(tipoUsuario == 'cliente' ? 'consultas_clientes' : 'consultas_profissionais')
-          .doc(consultaId)
-          .delete();
+          .collection('consulta')
+          .doc(widget.consultaId)
+          .update({
+        'titulo': tituloController.text,
+        'descricao': descricaoController.text,
+        'horaData': timestamp,
+        'nomeCliente': nomeClienteController.text,
+        'nomeProfissional': nomeProfissionalController.text,
+      });
 
-      await loadConsultas();
-    } catch (error) {
-      print('Erro ao excluir consulta: $error');
-    }
-  }
-
-  Future<void> editarConsulta(String consultaId) async {
-    try {
-      // Verifique o tipo de usuário antes de permitir a edição
-      if (tipoUsuario == 'profissional') {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => EditarConsultaScreen(consultaId: consultaId),
-          ),
-        );
-      } else {
-        // Se não for um profissional, exiba um alerta
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: const Text('Acesso Negado'),
-              content: const Text('Apenas profissionais podem editar consultas.'),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text('OK'),
-                ),
-              ],
-            );
-          },
-        );
-      }
+      print('Consulta editada com sucesso!');
     } catch (error) {
       print('Erro ao editar consulta: $error');
+    }
+  }
+
+  Future<void> adicionarConsulta() async {
+    try {
+      DateTime? dataSelecionada = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime.now(),
+        lastDate: DateTime(2101),
+      );
+
+      if (dataSelecionada != null) {
+        TimeOfDay? horaSelecionada = await showTimePicker(
+          context: context,
+          initialTime: TimeOfDay.now(),
+        );
+
+        if (horaSelecionada != null) {
+          DateTime dataHoraSelecionada = DateTime(
+            dataSelecionada.year,
+            dataSelecionada.month,
+            dataSelecionada.day,
+            horaSelecionada.hour,
+            horaSelecionada.minute,
+          );
+
+          horaDataController.text = DateFormat('yyyy-MM-dd HH:mm').format(dataHoraSelecionada);
+        }
+      }
+    } catch (error) {
+      print('Erro ao selecionar data e hora: $error');
+    }
+  }
+
+  Future<void> enviarConsulta() async {
+    try {
+      DateTime dataHora = DateTime.parse(horaDataController.text);
+      Timestamp timestamp = Timestamp.fromDate(dataHora);
+
+      await FirebaseFirestore.instance.collection('consulta').add({
+        'titulo': tituloController.text,
+        'descricao': descricaoController.text,
+        'horaData': timestamp,
+        'nomeCliente': nomeClienteController.text,
+        'nomeProfissional': nomeProfissionalController.text,
+      });
+
+      print('Consulta adicionada com sucesso!');
+    } catch (error) {
+      print('Erro ao adicionar consulta: $error');
     }
   }
 
@@ -88,113 +122,62 @@ class ConsultasScreenState extends State<ConsultasScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Consultas'),
+        title: const Text('Editar Consulta'),
       ),
       body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'Consultas',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 16.0),
-              ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: consultas.length,
-                itemBuilder: (context, index) {
-                  final consulta = consultas[index];
-                  return Card(
-                    elevation: 3.0,
-                    margin: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: ListTile(
-                      title: Text('ID: ${consulta['id']}'),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Título: ${consulta['titulo']}'),
-                          Text('Descrição: ${consulta['descricao']}'),
-                          Text('Data e Hora: ${consulta['horaData']}'),
-                          Text('Nome do Cliente: ${consulta['nomeCliente']}'),
-                          Text(
-                              'Nome do Profissional: ${consulta['nomeProfissional']}'),
-                        ],
-                      ),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {
-                              excluirConsulta(consulta['id']);
-                            },
-                            child: const Text('Excluir'),
-                            // Desabilita o botão se o usuário não for um profissional
-                            style: tipoUsuario != 'profissional'
-                                ? ButtonStyle(
-                                    backgroundColor: MaterialStateProperty.all(Colors.grey),
-                                  )
-                                : null,
-                          ),
-                          const SizedBox(width: 8.0),
-                          ElevatedButton(
-                            onPressed: () {
-                              editarConsulta(consulta['id']);
-                            },
-                            child: const Text('Editar'),
-                            // Desabilita o botão se o usuário não for um profissional
-                            style: tipoUsuario != 'profissional'
-                                ? ButtonStyle(
-                                    backgroundColor: MaterialStateProperty.all(Colors.grey),
-                                  )
-                                : null,
-                          ),
-                        ],
-                      ),
+        child: Center(
+          child: Container(
+            constraints: const BoxConstraints(maxWidth: 400),
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Center(
+                  child: Text(
+                    'Editar Consulta:',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
-                  );
-                },
-              ),
-              const SizedBox(height: 16.0),
-              FloatingActionButton(
-                onPressed: () {
-                  // Verifique o tipo de usuário antes de permitir a adição
-                  if (tipoUsuario == 'profissional') {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => AdicionarConsultaScreen(),
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                Form(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: tituloController,
+                        decoration: const InputDecoration(labelText: 'Título:'),
                       ),
-                    );
-                  } else {
-                    // Se não for um profissional, exiba um alerta
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: const Text('Acesso Negado'),
-                          content: const Text('Apenas profissionais podem adicionar consultas.'),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                              child: const Text('OK'),
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  }
-                },
-                child: const Icon(Icons.add),
-              ),
-            ],
+                      TextFormField(
+                        controller: descricaoController,
+                        decoration: const InputDecoration(labelText: 'Descrição:'),
+                      ),
+                      TextFormField(
+                        controller: horaDataController,
+                        decoration: const InputDecoration(labelText: 'Data e Hora:'),
+                        onTap: adicionarConsulta,
+                      ),
+                      TextFormField(
+                        controller: nomeClienteController,
+                        decoration: const InputDecoration(labelText: 'Nome do Cliente:'),
+                      ),
+                      TextFormField(
+                        controller: nomeProfissionalController,
+                        decoration: const InputDecoration(labelText: 'Nome do Profissional:'),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: editarConsultaExistente,
+                    child: const Text('Salvar Edições'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
