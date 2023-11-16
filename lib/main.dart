051973109fbaab2b7751b79a08577befedd6c5e5
Gabriel Assys Mendes Oliveira <110845125+GABRIEL-ASSYS@users.noreply.github.com>
2023-11-16 12:15:43 -0300

    corrigindo erros do firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:health_arch/firebase_options.dart';
 import 'package:health_arch/screens/Home/home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    // ignore: avoid_print
+    print('Error: $e');
+  }
 
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp();
 
   runApp(MaterialApp(
     home: const HomeScreen(),
